d3f7cb984c8baff2792e874e69344e3030ed6412
Alexei Guevara <alexguev@gmail.com>
2013-09-29 00:52:40 -0400

    improved street extraction

--- src/contest/core.clj
+++ src/contest/core.clj
@@ -3,19 +3,25 @@
             [clojure.string :as s]
             [iota :as iota]))
 
-(defn extract-street-name [location1]
-    (-> location1
-        (s/replace #"\s(NORTH|SOUTH|EAST|WEST|E|W|N|S)$" "")
-        (s/replace #"\s(ST|STREET|AV|AVE|COURT|CRT|CT|RD)$" "")
-        (s/replace #"^\s*[\"\-!#'\(%*$\./]*" "")
-        (s/replace #"^\s*([\d\.\-/!$']+[ABC]*\s*)+" "")))
+(def suffixes #{"E" "W" "N" "S" "NORTH" "SOUTH" "EAST" "WEST"
+                "ST" "CT" "RD" "AV" "AVE" "CRT" "STREET" "COURT" "CRES"
+                "CRS" "ROAD" "AVENUE" "BLVD" "GARDEN" "GARDENS" "GRDNS" "CRESCENT"})
+
+(defn extract-street-name [^String location1]
+  (let [^StringBuilder sb (StringBuilder.)]
+    (doseq [^String part (s/split location1 #"\s+")]
+      (when-not (or (re-find #"\d" part)
+                    (<= (.length part) 2)
+                    (and (> (.length sb) 0) (contains? suffixes part)))
+        (doto sb (.append part) (.append " "))))
+    (.substring sb 0 (if (> (.length sb) 0)
+                       (dec (.length sb))
+                       (.length sb)))))
 
 (defn parse [line]
-  (let [parts (s/split line #",")]
-    [(let [street-name (get parts 7)]
-       (extract-street-name (s/upper-case street-name)))
-     (when-let [set-fine-amount (get parts 4)]
-       (Long/parseLong set-fine-amount))]))
+  (let [[_ _ _ _ set-fine-amount _ _ street-name] (s/split line #",")]
+    [(extract-street-name (s/upper-case street-name))
+     (when set-fine-amount (Integer/parseInt set-fine-amount))]))
 
 (defn tree-map [m]
   (doto
@@ -48,13 +54,4 @@
 (defn -main [& args]
   (time (sort-parking-tickets "./resources/Parking_Tags_Data_2012.csv")))
 
-(-main)
-
-
-
-
-
-
-
-
-
+;(-main)
