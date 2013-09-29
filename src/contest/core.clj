7d9ea5a1437d718717f41a95c45078bae33ce86c
Alexei Guevara <alexguev@gmail.com>
2013-09-29 00:54:33 -0400

    some deconstructing

--- src/contest/core.clj
+++ src/contest/core.clj
@@ -4,21 +4,24 @@
             [iota :as iota]))
 
 (def suffixes #{"E" "W" "N" "S" "NORTH" "SOUTH" "EAST" "WEST"
-                "ST" "CT" "RD" "AV" "AVE" "CRT" "STREET" "COURT" "CRES" "CRS" "ROAD" "AVENUE" "BLVD" "GARDEN" "GARDENS" "GRDNS" "CRESCENT"})
+                "ST" "CT" "RD" "AV" "AVE" "CRT" "STREET" "COURT" "CRES"
+                "CRS" "ROAD" "AVENUE" "BLVD" "GARDEN" "GARDENS" "GRDNS" "CRESCENT"})
 
 (defn extract-street-name [^String location1]
   (let [^StringBuilder sb (StringBuilder.)]
     (doseq [^String part (s/split location1 #"\s+")]
-      (when-not (or (re-find #"\d" part) (<= (.length part) 2) (and (> (.length sb) 0) (contains? suffixes part)))
+      (when-not (or (re-find #"\d" part)
+                    (<= (.length part) 2)
+                    (and (> (.length sb) 0) (contains? suffixes part)))
         (doto sb (.append part) (.append " "))))
-    (.substring sb 0 (if (> (.length sb) 0) (dec (.length sb)) (.length sb)))))
+    (.substring sb 0 (if (> (.length sb) 0)
+                       (dec (.length sb))
+                       (.length sb)))))
 
 (defn parse [line]
-  (let [parts (s/split line #",")]
-    [(let [street-name (get parts 7)]
-       (extract-street-name (s/upper-case street-name)))
-     (when-let [set-fine-amount (get parts 4)]
-       (Long/parseLong set-fine-amount))]))
+  (let [[_ _ _ _ set-fine-amount _ _ street-name] (s/split line #",")]
+    [(extract-street-name (s/upper-case street-name))
+     (when set-fine-amount (Integer/parseInt set-fine-amount))]))
 
 (defn tree-map [m]
   (doto
